1d3120f318c0ee6bb5bcb7f56988e448cde746ec
skals317 <66448960+skals317@users.noreply.github.com>
2022-04-15 21:58:58 +0900

    main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.weather.go.kr/w/eqk-vol/search/korea.do?schOption=&xls=0&startTm=2022-01-01&endTm=2022-04-15&startSize=&endSize=&startLat=&endLat=&startLon=&endLon=&lat=&lon=&dist=&keyword=&dpType=a'
+        
+response = requests.get(url)
+    
+if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        time = soup.select_one('#excel_body > tbody > tr:nth-child(1) > td:nth-child(2) > span')
+        time = time.get_text()
+        mag = soup.select_one('#excel_body > tbody > tr:nth-child(1) > td:nth-child(3) > span')
+        mag = mag.get_text()
+        dep = soup.select_one('#excel_body > tbody > tr:nth-child(1) > td:nth-child(4) > span')
+        dep = dep.get_text()
+        maxint = soup.select_one('#excel_body > tbody > tr:nth-child(1) > td:nth-child(5) > span')
+        maxint = maxint.get_text()
+        area = soup.select_one('#excel_body > tbody > tr:nth-child(1) > td:nth-child(8) > span')
+        area = area.get_text()
+        map = soup.select_one('#excel_body > tbody > tr:nth-child(1) > td:nth-child(9) > a')
+        map = str(map)[9:86]
+
+        print("발생시각 : "+time)
+        print("규모 : M"+mag)
+        print("깊이 : "+dep+"km")
+        print("최대진도 : "+maxint)
+        print("발생지역"+area)
+        print("지도 : "+map)
+else : 
+        print(response.status_code)    
